4206cb79ff11bd06ac1bfd379b2f1123e010f84c
화해방 <bcl0206@naver.com>
2023-01-26 17:00:28 +0900

    error page complete

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -12,20 +12,20 @@ export default function Promotion() {
         alt='promotion_1'
       />
       <div className=' absolute top-0 opacity-40 w-full h-full bg-black'></div>
-      <div className='absolute top-16 left-10 text-white '>
-        <p className='text-xl mb-4'>
+      <div className='absolute top-16 left-10 '>
+        <p className='text-xl mb-4  text-white'>
           <span className='font-bold'>DEAL</span> OF THE WEEK
         </p>
-        <p className='text-9xl  mb-9'>
+        <p className='text-9xl  mb-9  text-white'>
           Stay Warm
           <br />
           And Trendy
         </p>
         <Countdown
           date={Date.now() + 100000000}
-          className='text-6xl text-shadow sm:text-shadow-sm md:text-shadow-md lg:text-shadow-lg xl:text-shadow-xl'
+          className='text-6xl text-shadow sm:text-shadow-sm md:text-shadow-md lg:text-shadow-lg xl:text-shadow-xl  text-white'
         />
-        <p className='text-xl mb-10'>
+        <p className='text-xl mb-10  text-white'>
           &nbsp;days &nbsp;&nbsp;&nbsp; hours &nbsp;&nbsp;&nbsp; mins
           &nbsp;&nbsp;&nbsp; sec
         </p>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function NotFound() {
-  return <div className='pt-14'>notFound</div>;
+  return (
+    <div className='pt-14 flex flex-col items-center justify-center h-screen'>
+      <img src='../image/main/error_1.jpg' alt='' className='w-96 h-96' />
+      <p>페이지를 불러올 수 없습니다. 관리자에게 문의 바랍니다.</p>
+      <p>이용에 불편을 드려 대단히 죄송합니다.</p>
+    </div>
+  );
 }
