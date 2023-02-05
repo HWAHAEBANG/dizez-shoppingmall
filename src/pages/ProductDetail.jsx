a8e28645aa7665aeb25e1c1c71eedbf00e9c287d
화해방 <bcl0206@naver.com>
2023-02-05 16:39:31 +0900

    not last

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -85,10 +85,6 @@ export default function ProductDetail() {
   };
 
   console.log(image);
-  // console.log(test);
-  // const {
-  //   dibbsQuery: { data: dibbsProducts },
-  // } = useDibbs();
 
   return (
     <div className='pt-24 mb-10 xl:mb-0 flex flex-col xl:flex-row xl:p-20 font-["Raleway"] '>
